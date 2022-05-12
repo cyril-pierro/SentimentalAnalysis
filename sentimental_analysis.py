567cffb7476c1ee000597f0c4c49541d119aa292
Peter Badasu <pbadasu@quantumgroupgh.com>
2022-05-12 12:31:33 +0000

    project: add dependant modules

---
+++ sentimental_analysis.py
@@ -0,0 +1,4 @@
+from tensorflow.keras.preprocessing.text import one_hot
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Embedding, Conv1D, Dropout, GlobalMaxPool1D
