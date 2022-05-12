674da60128ab2c5d70fcd0843b908932e7e599d1
Peter Badasu <pbadasu@quantumgroupgh.com>
2022-05-12 13:16:39 +0000

    project: add Embedding layer to model

--- model.py
+++ model.py
@@ -2,8 +2,11 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Embedding, Conv1D, Dropout, GlobalMaxPool1D
 
 
-def TextCnn():
+def TextCnn(vocabulary_size, max_value_length):
     """
     Text Sentimental Convolutional Network
     """
     embedding_vector_size = 20
+    model = Sequential()
+    model.add(Embedding(vocabulary_size, embedding_vector_size,
+              input_length=max_value_length, name='embedding'))
